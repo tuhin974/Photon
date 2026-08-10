05a2641d85c80c9e3a9e4f9692f5b4aeb3768092
Manish Gowda <manishgowda025@gmail.com>
2026-08-10 18:16:21 +0530

    Add multiple stock streaming

--- backend/streamer.py
+++ backend/streamer.py
@@ -1,8 +1,24 @@
 import random
 
+STOCKS = {
+    "AAPL": 150,
+    "GOOGL": 2800,
+    "TSLA": 700,
+    "MSFT": 420
+}
+
 
 def generate_stock_data():
+    ticker = random.choice(list(STOCKS.keys()))
+
+    base_price = STOCKS[ticker]
+
+    price = round(
+        random.uniform(base_price - 5, base_price + 5),
+        2
+    )
+
     return {
-        "ticker": "AAPL",
-        "price": round(random.uniform(145, 155), 2)
+        "ticker": ticker,
+        "price": price
     }
